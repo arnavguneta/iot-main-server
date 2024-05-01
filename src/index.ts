bbb36dc8a55b2d36927a82a62420b4cb382e918b
Arnav <arnavguneta@gmail.com>
2024-05-01 01:54:37 -0400

    Add current ip

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import lampRoutes from '@routes/lampRoutes';
 import IOService from '@services/ioService';
+import os from 'os';
 
 dotenv.config();
 
-const port = process.env.PORT || 3001;
+const port: number = parseInt(process.env.PORT as string, 10) || 3001;
 const app: Express = express();
 
 const { server } = IOService.initializeIO(app);
@@ -15,5 +16,10 @@ console.log(port);
 app.use(`${process.env.API_ENDPOINT}/lamp`, lampRoutes);
 
 server.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    const networkInterfaces = os.networkInterfaces();
+    const interfaces = networkInterfaces['en0'];
+    const addresses = interfaces?.filter((iface) => iface.family === 'IPv4');
+    const ipv4Address = addresses && addresses.length > 0
+        ? addresses[0].address : 'localhost';
+    console.log(`[server]: Server is running at http://${ipv4Address}:${port}`);
 });
